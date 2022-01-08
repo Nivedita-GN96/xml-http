18425a52307aabe56c3c8e4d35cabe7db9b14bc7
Nivedita-GN96 <niveditagouda1996@gmail.com>
2022-01-08 22:22:37 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var request=new XMLHttpRequest();
+request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json',true);
+request.send();
+request.onload=function(){
+    var data=JSON.parse(request.response);
+    console.log(data);
+    for(i=0;i<data.length;i++){
+        console.log(data[i].name)
+
+    }
+    for(i=0;i<flags;i++){
+        console.log(data[i].name)
+
+    }
+        
+    }
